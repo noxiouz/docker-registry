a5fd05e02a96a78efc740a239924ce2b44cd65ac
Brian Bland <brian.bland@docker.com>
2014-10-13 14:41:26 -0700

    Removes unnecessary pathing boilerplate from driver.go

--- contrib/golang_impl_ng/driver/driver.go
+++ contrib/golang_impl_ng/driver/driver.go
@@ -15,26 +15,6 @@ type Driver interface {
 	Delete(path string) error
 }
 
-func ImageManifestPath(imageId string) string {
-	return fmt.Sprintf("images/%s/manifest.json", imageId)
-}
-
-func ImagePrivatePath(imageId string) string {
-	return fmt.Sprintf("images/%s/_private", imageId)
-}
-
-func ImageDeletionPath(imageId string) string {
-	return fmt.Sprintf("images/%s/_deleted", imageId)
-}
-
-func ImageLayerPath(layerTarsum string) string {
-	return fmt.Sprintf("layers/%s/layer", layerTarsum)
-}
-
-func LayerChecksumPath(layerTarsum string) string {
-	return fmt.Sprintf("layers/%s/checksum", layerTarsum)
-}
-
 type PathNotFoundError struct {
 	Path string
 }
